7d97c18efa8d9cf0b36259a2d08bad69b479bca2
prins_____18 <100407578+abrahamThom@users.noreply.github.com>
2023-10-13 16:15:56 +0530

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,5 @@
+import math
+a=int(input("Enter Number a:"))
+b=int(input("Enter number b:"))
+print("GCD of",a,"and",b,"is",math.gcd(a,b))
+
---
+++ mergedict.py
@@ -0,0 +1,4 @@
+dict1={1 :"apple",2:"Watermelone",3 : "Painapple"}
+dict2={4 :"plum",5 :"Orange"}
+dict1.update(dict2)
+print(dict1)
